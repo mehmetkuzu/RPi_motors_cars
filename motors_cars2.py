6da0ba3a2236218a89cfcb8fed02ee58a0076843
mehmetkuzu <mehmetkuzulugil@gmail.com>
2020-01-12 19:54:53 +0300

    wheels data corrected

--- motors_cars2.py
+++ motors_cars2.py
@@ -205,8 +205,8 @@ def getStandart4WheelCar2():
     pinsRight = motor_pins(17,18,23)
     pinsLeft = motor_pins(22,27,24)
 
-    pinsRight2 = motor_pins(25,4,8)
-    pinsLeft2 = motor_pins(10,9,11)
+    pinsLeft2 = motor_pins(25,4,8)
+    pinsRight2 = motor_pins(10,9,11)
 
     # MOTOR1IN1 = 17
     # MOTOR1IN2 = 18
